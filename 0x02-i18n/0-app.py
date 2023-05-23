1d2614b3b7b7451e29be97ee36a70b563eb5b259
saad-out <outsaad03@gmail.com>
2023-05-23 11:06:16 +0100

    basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module creates a Flask app.
+"""
+from flask import (
+    Flask,
+    render_template
+)
+
+
+app: Flask = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    This function returns a template.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
